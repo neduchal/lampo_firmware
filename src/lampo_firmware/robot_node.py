135b103275fafceb7b288650bc0a36504f727d50
Petr Neduchal <neduchal@gmail.com>
2022-08-15 13:16:50 +0200

    Pridani moznosti posilat prikaz kazdemu motoru zvlast.

--- src/lampo_firmware/robot_node.py
+++ src/lampo_firmware/robot_node.py
@@ -5,6 +5,7 @@ from display import Display
 from motors import RobotMotors
 from geometry_msgs.msg import Twist
 from std_msgs.msg import String
+from std_msgs.msg import Int32
 
 class Robot:
 
@@ -12,6 +13,8 @@ class Robot:
         self.motors = RobotMotors()
         self.display = Display()
         self.control_subscriber = rospy.Subscriber("/cmd_vel", Twist, callback=self.control_callback, queue_size=10)
+        self.left_motor_subscriber = rospy.Subscriber("/cmd_left_motor", Int32, callback=self.left_motor_callback, queue_size=10)
+        self.right_motor_subscriber = rospy.Subscriber("/cmd_right_motor", Int32, callback=self.right_motor_callback, queue_size=10)
         self.display_subscriber = rospy.Subscriber("/display", String, callback=self.display_callback, queue_size=10)
 
     def display_callback(self, msg):
@@ -19,30 +22,53 @@ class Robot:
         self.display.push()
         pass
 
-    def control_callback(self, msg):
-        linear = msg.linear.x
-        angular = msg.angular.z
+    def constraint_motor_value(self, value):
+        value = max(min(value, 100), -100)   
+        motor_direction = "forward" if value >= 0 else "backward"
+        return motor_direction, abs(value)
 
+    def left_motor_callback(self, msg):
+        motor_direction, value = self.constraint_motor_value(msg.data)
+        self.motorRun(0, motor_direction, value)
+
+    def right_motor_callback(self, msg):
+        motor_direction, value = self.constraint_motor_value(msg.data)
+        self.motorRun(1, motor_direction, value)
+
+    def compute_motor_values(self, linear, angular):
+        Ml = 0
+        Mr = 0
         if (linear != 0):
             Ml = (1 - angular) * linear * 100
             Mr = (1 + angular) * linear * 100
         else:
             Ml = - angular * 100
-            Mr = angular * 100
+            Mr = angular * 100    
+        return Ml, Mr
 
-        Ml = max(min(Ml, 100), -100)
-        Mr = max(min(Mr, 100), -100)
+    def control_callback(self, msg):
+        Ml, Mr = self.compute_motor_values(msg.linear.x, msg.angular.z)
 
-        MlDir = "forward" if Ml >= 0 else "backward"
-        MrDir = "forward" if Mr >= 0 else "backward"
-
+        MlDir, Ml = self.constraint_motor_value(Ml)
+        MrDir, Mr = self.constraint_motor_value(Mr)
         self.motors.motorRun(0, MlDir, abs(Ml))
         self.motors.motorRun(1, MrDir, abs(Mr))
+
+    def run(self):
+        r = rospy.Rate(0.25) # 10hz 
+        i = 0
+        while not rospy.is_shutdown():
+            if i % 2 == 1:
+                self.display.drawIP()
+            else:
+                self.display.push()
+            i += 1
+            r.sleep()
 
 if __name__ == "__main__":
     rospy.init_node("robot_node")
     R = Robot()
-    rospy.spin()
+    R.run()
     R.display.clear()
 
 
